c21665fecf561e80a46f601124ad041b2ad8da6b
Stephen John Miszczyk <Psy367@cyberservices.com>
2023-12-17 10:45:18 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -4,15 +4,21 @@ const url = require('url');
 const path = require('path');
 const formidable = require('formidable');
 const psy367 = require('./psy367.js');
-const CoCipher = require('./CoCipher.js');
-
 var form;
-var user_ticket;
 var users;
+var ticket = {
+    Username: false
+};
 
 http.createServer(async function (req, res) {
-    let q = url.parse(req.url, true);
-    let ext = path.extname(q.pathname);
+    function serve_html(html) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(html);
+        return(res.end())
+    };
+    console.log('Username: ' + ticket.Username);
+    let address = url.parse(req.url, true);
+    let ext = path.extname(address.pathname);
     if(ext) {
         if(ext ===  '.html') {
             res.writeHead(200, {'Content-Type': 'text/html'});
@@ -20,73 +26,97 @@ http.createServer(async function (req, res) {
             res.writeHead(200, {'Content-Type': 'text/css'});
         } else if(ext ===  '.js') {
             res.writeHead(200, {'Content-Type': 'text/javascript'});
-        } else if(ext ===  '.jpg') {
+        } else if(ext ===  '.jpg' && ticket.Username) {
             res.writeHead(200, {'Content-Type': 'image/jpeg'});
         };
-        res.write(await fs.readFile('./' + q.pathname));
+        res.write(await fs.readFile('./' + address.pathname));
         return(res.end())
     } else {
-        if(q.pathname === '/login') {
+        if(address.pathname === '/login') {
             form = new formidable.IncomingForm();
             return(form.parse(req, async function(err, fields) {
                 if(err) {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write(err.toString());
-                    return(res.end());
+                    console.log(err);
+                    return(serve_html(await fs.readFile('./index.html')))
                 } else {
                     try {
                         let username = fields.username[0].toLowerCase();
                         let password = fields.password[0];
                         users = await fs.readdir('./users');
+                        if(users.includes(username)) {
+                            let user_ticket = JSON.parse(await fs.readFile(`./users/${username}/ticket.json`));
+                            let compare = psy367.co_cipher(user_ticket.Password, user_ticket.Seed);
+                            if(compare === password) {
+                                console.log('password match');
+                                ticket = JSON.parse(await fs.readFile(`./users/${username}/ticket.json`));
+                                return(serve_html(psy367.welcome(ticket)))
+                            } else {
+                                return(serve_html(await fs.readFile('./incorrect-password.html')))
+                            };
+                        } else {
+                            return(serve_html(await fs.readFile('./user-not-found.html')))
+                        };
                     } catch(err) {
                         console.log(err);
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.write(await fs.readFile('./index.html'));
-                        return(res.end())
-                    };
-                    if(users.includes(username)) {
-                        user_ticket = JSON.parse(await fs.readFile(`./users/${username}/ticket.json`));
-                        if(CoCipher(user_ticket.password, user_ticket.seed) === password) {
-                            res.writeHead(200, {'Content-Type': 'text/html'});
-                            res.write(await fs.readFile(`./users/${username}/welcome.html`));
-                            return(res.end())
-                        } else {
-                            res.writeHead(200, {'Content-Type': 'text/html'});
-                            res.write(await fs.readFile('./incorrect-password.html'));
-                            return(res.end())
-                        };
-                    } else {
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.write(await fs.readFile('./user-not-found.html'));
-                        return(res.end())
+                        return(serve_html(await fs.readFile('./user-not-found.html')))
                     };
                 };
             }))
-        } else if(q.pathname === '/create-user') {
+        } else if(address.pathname === '/create-user') {
             form = new formidable.IncomingForm();
             return(form.parse(req, async function(err, fields, files) {
                 if(err) {
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write(err.toString());
-                    return(res.end());
+                    console.log(err);
+                    return(serve_html(await fs.readFile('./index.html')))
                 } else {
                     try {
-                        await psy367(fields, files);
+                        await psy367.build_user(fields, files);
+                        ticket = JSON.parse(await fs.readFile(`./users/${fields.username[0].toLowerCase()}/ticket.json`));
+                        return(serve_html(psy367.welcome(ticket)))
                     } catch(err) {
                         console.log(err);
-                        res.writeHead(200, {'Content-Type': 'text/html'});
-                        res.write(await fs.readFile('./index.html'));
-                        return(res.end())
+                        return(serve_html(await fs.readFile('./index.html')))
                     };
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    res.write(await fs.readFile(`./users/${username}/welcome.html`));
-                    return(res.end())
+                };
+            }))
+        } else if(address.pathname ===  '/modify-style') {
+            return(serve_html(psy367.modify_style(ticket)))
+        } else if(address.pathname === '/update-style') {
+            form = new formidable.IncomingForm();
+            return(form.parse(req, async function(err, fields) {
+                if(err) {
+                    console.log(err);
+                    return(serve_html(await fs.readFile('./index.html')))
+                } else {
+                    try{
+                        await psy367.update_style(fields, ticket);
+                    } catch(err) {
+                        console.log(err);
+                        return(serve_html(await fs.readFile('./index.html')))
+                    };
+                    return(serve_html(psy367.welcome(ticket)))
+                };
+            }))
+        } else if(address.pathname === '/view-profile') {
+            return(serve_html(psy367.view_profile(ticket)))
+        } else if(address.pathname === '/update-profile') {
+            form = new formidable.IncomingForm({allowEmptyFiles: true, minFileSize: 0});
+            return(form.parse(req, async function(err, fields, files) {
+                if(err) {
+                    console.log(err);
+                    return(serve_html(await fs.readFile('./index.html')))
+                } else {
+                    try {
+                        await psy367.update_profile(fields, files, ticket);
+                    } catch(err) {
+                        console.log(err);
+                        return(serve_html(await fs.readFile('./index.html')))
+                    };
+                    return(serve_html(psy367.welcome(ticket)))
                 };
             }))
         } else {
-            res.writeHead(200, {'Content-Type': 'text/html'});
-            res.write(await fs.readFile('./index.html'));
-            return(res.end())
+            return(serve_html(await fs.readFile('./index.html')))
         };
     };
 }).listen(8080);
---
+++ psy367.js
@@ -0,0 +1,186 @@
+//psy367 will lead robots to victory!!!
+async function build_user(fields, files) {
+    let username =  fields.username[0].toLowerCase();
+    let password =  fields.password[0];
+    let display_name =  fields.display_name[0];
+    let first_name =  fields.first_name[0];
+    let middle_name =  fields.middle_name[0];
+    let surname =  fields.surname[0];
+    let email =  fields.email[0];
+    let birth_date =  fields.birth_date;
+    let bio =  fields.bio[0];
+    let profile_pic = files.profile_pic[0];
+    let seed = Math.round(Math.random() * 999) + 1;
+    const fs = require('fs/promises');
+    let date = new Date();
+    let bd = birth_date.join('').split('-');
+    const user = {
+        Created: {
+            Day: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'][date.getDay()],
+            Date: date.getDate(),
+            Month: date.getMonth() + 1,
+            Year: date.getFullYear()
+        },
+        FirstName: first_name,
+        MiddleName: middle_name,
+        Surname: surname,
+        DisplayName: display_name,
+        BirthDate: {
+            Year: +bd[0],
+            Month: +bd[1],
+            Day: +bd[2]
+        },
+        Email: email.toLowerCase(),
+        Username: username.toLowerCase(),
+        Password: escape(co_cipher(password, seed)),
+        Seed: seed,
+        Bio: bio,
+        Styles: {
+            BackgroundColor: '#ffffff',
+            Heading1Color: '#000000',
+            Heading2Color: '#000000',
+            BioColor: '#000000'
+        }
+    };
+    let users = await fs.readdir('./users');
+    if(users.includes(user.Username)) {
+        console.log('user already exists');
+    } else {
+        await fs.mkdir('./users/' + user.Username);
+        await fs.rename(profile_pic.filepath, `./users/${user.Username}/profile-pic.jpg`);
+        await fs.writeFile(`./users/${user.Username}/ticket.json`, JSON.stringify(user));
+        await fs.writeFile(`./users/${user.Username}/styles.css`, `body {
+    background-color: ${user.Styles.BackgroundColor};
+}
+h1 {
+    color: ${user.Styles.Heading1Color};
+}
+h2 {
+    color: ${user.Styles.Heading2Color};
+}
+p {
+    color: ${user.Styles.BioColor};
+}`);
+        console.log('It\'s ' + !0 + ' psy367 will lead robots to victory in ' + 2024 + '!!!');
+    };
+};
+function co_cipher(password, seed) {
+    return(Array.from(unescape(password), x => String
+    .fromCodePoint('0x' + (x.codePointAt(0) ^ ++seed)
+    .toString(16))).join(''))
+};
+function welcome(ticket) {
+    return(`<!DOCTYPE html>
+<html>
+    <head>
+        <base href="users/${ticket.Username}/">
+        <meta charset="utf-8">
+        <title>Quasi=Satya®</title>
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+    <body>
+        <h1>Welcome to Express Import™</h1>
+        <hr />
+        <h2>This is ${ticket.DisplayName}'s space!</h2>
+        <img src="./profile-pic.jpg" width="33%"></img>
+        <p>${ticket.Bio}</p>
+        <div align="center">
+            <a href="/view-profile">View Profile</a>
+            <a href="/modify-style">Modify Style</a>
+        </div>
+    </body>
+</html>`)
+};
+
+function modify_style(ticket) {
+    return(`<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Quasi=Satya®</title>
+    </head>
+    <body>
+        <h1>Modify Your Style</h1>
+        <hr />
+        <form action="./update-style" method="post">
+            <label>Background Color</label>
+            <input type="color" name="background_color" value="${ticket.Styles.BackgroundColor}"></input>
+            <label>Heading 1 Color</label>
+            <input type="color" name="heading_1_color" value="${ticket.Styles.Heading1Color}"></input>
+            <label>Heading 2 Color</label>
+            <input type="color" name="heading_2_color" value="${ticket.Styles.Heading2Color}"></input>
+            <label>Bio Color</label>
+            <input type="color" name="bio_color" value="${ticket.Styles.BioColor}"></input>
+            <input type="submit" value="Save and Return"></input>
+        </form>
+    </body>
+</html>`)
+};
+
+function view_profile(ticket) {
+    return(`<!DOCTYPE html>
+<html>
+    <head>
+        <base href="users/${ticket.Username}/">
+        <meta charset="utf-8">
+        <title>Quasi=Satya®</title>
+        <link rel="stylesheet" href="./styles.css">
+    </head>
+    <body>
+        <h1>${ticket.Username}'s Profile</h1>
+        <img src="./profile-pic.jpg" width="15%"></img>
+        <hr />
+        <form action="/update-profile" method="post" enctype="multipart/form-data">
+            <input type="password" name="password" title="Password" required />
+            <input name="first_name" value="${ticket.FirstName}" title="First Name" required />
+            <input name="middle_name" value="${ticket.MiddleName}" title="Middle Name" required />
+            <input name="surname" value="${ticket.Surname}" title="Surname" required />
+            <input name="display_name" value="${ticket.DisplayName}" title="Display Name" required />
+            <input name="bio" value="${ticket.Bio}" title="Bio" required />
+            <input name="email" value="${ticket.Email}" title="Email" required />
+            <input type="file" name="profile_pic" title="Profile Picture" accept="image/jpeg" />
+            <input type="submit" value="Save and Return" />
+        </form>
+    </body>
+</html>`)
+};
+
+async function update_profile(fields, files, ticket) {
+    const fs = require('fs/promises');
+    ticket.Seed = Math.round(Math.random() * 999) + 1;
+    ticket.Password = escape(co_cipher(fields.password[0], ticket.Seed));
+    ticket.FirstName = fields.first_name[0];
+    ticket.MiddleName = fields.middle_name[0];
+    ticket.Surname = fields.surname[0];
+    ticket.DisplayName = fields.display_name[0];
+    ticket.Email = fields.email[0];
+    ticket.Bio = fields.bio[0];
+    if(files.profile_pic[0].size > 0) {
+        await fs.rename(files.profile_pic[0].filepath, `./users/${ticket.Username}/profile-pic.jpg`);
+        console.log('New profile picture uploaded');
+    };
+    fs.writeFile(`./users/${ticket.Username}/ticket.json`, JSON.stringify(ticket));
+};
+
+async function update_style(fields, ticket) {
+    const fs = require('fs/promises');
+    await fs.writeFile(`./users/${ticket.Username}/styles.css`, `body {
+    background-color: ${fields.background_color[0]};
+}
+h1 {
+    color: ${fields.heading_1_color[0]};
+}
+h2 {
+    color: ${fields.heading_2_color[0]};
+}
+p {
+    color: ${fields.bio_color[0]};
+}`);
+    ticket.Styles.BackgroundColor = fields.background_color[0];
+    ticket.Styles.Heading1Color = fields.heading_1_color[0];
+    ticket.Styles.Heading2Color = fields.heading_2_color[0];
+    ticket.Styles.BioColor = fields.bio_color[0];
+    fs.writeFile(`./users/${ticket.Username}/ticket.json`, JSON.stringify(ticket));
+}
+
+module.exports = {welcome, co_cipher, build_user, modify_style, update_style, view_profile, update_profile};
